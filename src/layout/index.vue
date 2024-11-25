183cce7d5baf043abc7c97a961d1b2327f7395b7
L1510564429 <1510564429@qq.com>
2024-11-25 18:11:51 +0800

    初始化后台管理项目

---
+++ src/layout/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <el-container class="layout-container">
+    <el-aside width="200px">
+      <!-- 侧边栏 -->
+      <el-menu
+        :default-active="activeMenu"
+        class="el-menu-vertical"
+        background-color="#304156"
+        text-color="#bfcbd9"
+        active-text-color="#409EFF"
+      >
+        <el-menu-item index="/dashboard">
+          <el-icon><HomeFilled /></el-icon>
+          <span>首页</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    
+    <el-container>
+      <el-header>
+        <!-- 顶部导航栏 -->
+        <div class="header-container">
+          <div class="left">
+            <el-icon><Expand /></el-icon>
+          </div>
+          <div class="right">
+            <el-dropdown>
+              <span class="el-dropdown-link">
+                管理员<el-icon class="el-icon--right"><arrow-down /></el-icon>
+              </span>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item>个人信息</el-dropdown-item>
+                  <el-dropdown-item>退出登录</el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </div>
+        </div>
+      </el-header>
+      
+      <el-main>
+        <!-- 主要内容区域 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { HomeFilled, Expand } from '@element-plus/icons-vue'
+
+const route = useRoute()
+const activeMenu = computed(() => route.path)
+</script>
+
+<style scoped>
+.layout-container {
+  height: 100vh;
+}
+
+.el-header {
+  background-color: #fff;
+  border-bottom: 1px solid #e6e6e6;
+}
+
+.header-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+}
+
+.el-menu-vertical {
+  height: 100%;
+}
+
+.el-main {
+  background-color: #f0f2f5;
+}
+</style> 
---
+++ src/views/login/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="login-container">
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+    >
+      <h3 class="title">音乐后台管理系统</h3>
+      <el-form-item prop="username">
+        <el-input
+          v-model="loginForm.username"
+          placeholder="用户名"
+          type="text"
+        />
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input
+          v-model="loginForm.password"
+          placeholder="密码"
+          type="password"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          :loading="loading"
+          type="primary"
+          style="width: 100%"
+          @click="handleLogin"
+        >
+          登录
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const loading = ref(false)
+
+const loginForm = reactive({
+  username: '',
+  password: ''
+})
+
+const loginRules = {
+  username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+  password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+}
+
+const handleLogin = () => {
+  loading.value = true
+  // TODO: 实现登录逻辑
+  setTimeout(() => {
+    router.push('/')
+    loading.value = false
+  }, 1000)
+}
+</script>
+
+<style scoped>
+.login-container {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #2d3a4b;
+}
+
+.login-form {
+  width: 400px;
+  padding: 30px;
+  background: #fff;
+  border-radius: 4px;
+}
+
+.title {
+  text-align: center;
+  margin-bottom: 30px;
+}
+</style> 
